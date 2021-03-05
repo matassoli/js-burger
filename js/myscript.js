c17d48f838610bd28b7f136b763e572572449762
Nicolò Matassoli <nico.matassoli@gmail.com>
2021-03-05 22:16:13 +0100

    html fix + controllo iniziale

--- js/myscript.js
+++ js/myscript.js
@@ -1,1 +1,14 @@
-console.log("ciao")
+var tot = document.getElementById("tot"),
+  burgerName = document.getElementById("input__name");
+
+
+tot.addEventListener("click",
+  function() {
+
+    if (burgerName.value != "") {
+
+    } else {
+      alert("Inserisci il nome del panino");
+    }
+  }
+);
